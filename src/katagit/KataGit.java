7ee178e0bd410fd79c710b873d44681b539711a8
Entrar <Entrar@pc-aula11.dis.ulpgc.es>
2014-10-13 15:21:32 +0100

    Project created

---
+++ src/katagit/KataGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package katagit;
+
+/**
+ *
+ * @author Entrar
+ */
+public class KataGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
