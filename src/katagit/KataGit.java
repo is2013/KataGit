1eb8d6700781639fefee689b61653f7ab2197ff5
Entrar <Entrar@pc-aula11.dis.ulpgc.es>
2014-10-13 15:25:13 +0100

    Comments removed in Main class

--- src/katagit/KataGit.java
+++ src/katagit/KataGit.java
@@ -3,7 +3,7 @@ package katagit;
 public class KataGit {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("fasfas");
     }
     
 }
